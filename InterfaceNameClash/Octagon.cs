2dfe6c8fba263d393f13e57a54024aea96716130
Alexandr <tolstov.alexandr.85@gmail.com>
2019-06-14 17:53:31 +0600

    Add project files.

---
+++ InterfaceNameClash/Octagon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNameClash
+{
+    class Octagon : IDrawToForm, IDrawToMemory, IDrawToPrinter
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing the Octagon...");
+        }
+    }
+}
---
+++ InterfaceNameClash/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNameClash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Interface Name Claches *****\n");
+            Octagon oct = new Octagon();
+
+            IDrawToForm itfForm = (IDrawToForm)oct;
+            itfForm.Draw();
+            IDrawToForm itfForm = (IDrawToForm)oct;
+        }
+    }
+}
